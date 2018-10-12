dbf5eebc1b291ee13896d633a629a516d3891527
nede64 <ned.uchiha@gmail.com>
2018-10-12 21:48:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/zothix/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.zothix.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button g;
+Button f;
+// ijasd fisa fdijs dfsad jf
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
